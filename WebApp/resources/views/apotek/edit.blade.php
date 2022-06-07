71b3d3a2222162064045853524d0c3beb50a6bcf
Adika-13 <76163286+Adika-13@users.noreply.github.com>
2022-06-07 16:11:50 +0700

    Update edit.blade.php

--- WebApp/resources/views/apotek/edit.blade.php
+++ WebApp/resources/views/apotek/edit.blade.php
@@ -4,49 +4,70 @@
     <h1>{{ $title }}</h1>
 </div>
 
-<div class="section-body">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div style="width: 100%; margin: 10px" id='myDiv'>
-                    <form>
+@if ($errors->any())
+<div class="alert alert-danger" style="padding-bottom: 2px">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="apotek/{{ $apotek->id }}/edit" method="post" enctype="multipart/form-data">
+    @csrf
+    <div class="section-body">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Edit Foto Apotek</h5>
+                    </div>
+                    <div class="card-body d-flex justify-content-center">
+                        <div id="image-preview" class="image-preview" style="height: 250px">
+                            <label for="image-upload" id="image-label">Choose File</label>
+                            <input type="file" accept="image/*" name="picture" id="image-upload" value="{{old('picture') ?? $id -> picture}}"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div style="width: 100%; margin: 10px" id='myDiv'>
+
                         <div class="row mb-3">
                             <label class="col-sm-2 col-form-label">nama apotek</label>
                             <div class="col-sm-10">
-                                <p></p>
+                                <input type="text" class="form-control" name="name" value="{{old('name') ?? $id -> name}}">
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">komposisi</label>
+                            <label class="col-sm-2 col-form-label">Alamat</label>
                             <div class="col-sm-10">
-                                <textarea class="form-control" name="komposisi"></textarea>
+                                <textarea class="form-control" name="address" >"{{old('address') ?? $id -> address}}"</textarea>
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">khasiat</label>
+                            <label class="col-sm-2 col-form-label">Jam Operasi</label>
                             <div class="col-sm-10">
-                                <textarea class="form-control" name="komposisi"></textarea>
+                                <input type="text" class="form-control" name="jam_operasi" value="{{old('jam_operasi') ?? $id -> jam_operasi}}">
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">aturan Pakai</label>
+                            <label class="col-sm-2 col-form-label">No Telepon</label>
                             <div class="col-sm-10">
-                                <textarea class="form-control" name="aturanPakai"></textarea>
+                                <input type="number" class="form-control" name="telp" value="{{old('telp') ?? $id -> telp}}">
                             </div>
                         </div>
-                        <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">peringatan</label>
-                            <div class="col-sm-10">
-                                <textarea class="form-control" name="peringatan"></textarea>
-                            </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-success">Simpan</button>
                         </div>
+                    </div>
                 </div>
-                </form>
             </div>
         </div>
     </div>
-</div>
-</div>
+</form>
 
 <script src='https://cdn.plot.ly/plotly-2.8.3.min.js'></script>
 @endsection
