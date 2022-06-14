de4999ab021bb7645e9239b281c7f64ca0e47425
Imam Rafiif Arrazaan <karrazaan@gmail.com>
2022-06-14 08:52:25 +0700

    obat and apotek
    fix obat detail modal and add map apotek

--- WebApp/resources/views/apotek/detail.blade.php
+++ WebApp/resources/views/apotek/detail.blade.php
@@ -23,7 +23,7 @@
                     @endif
                 </div>
                 <div class="card-body">
-                    <label>Name</label>
+                    <label>Nama Apotek</label>
                     <p>{{ $id->name }}</p>
                     <label>Alamat</label>
                     <p>{{ $id->address }}</p>
@@ -31,9 +31,84 @@
                     <p>{{ $id->jam_operasi }}</p>
                     <label>No telepon</label>
                     <p>{{ $id->telp }}</p>
+                    <div class="form-group row mb-4">
+                        <label class="col-sm-2 col-form-label">Lokasi</label>
+                        <div class="col-sm-10">
+                            <div id='map' style='width: 100%; height: 500px;'></div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.7.0/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.7.0/mapbox-gl.css' rel='stylesheet' />
+
+<link rel="stylesheet"
+    href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css" type="text/css">
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
+
+<script>
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic2hpbnlxMTEiLCJhIjoiY2ptY3d3OGxsMTA1dDNsbnl4OXJ1cHpkeCJ9.7fp_UEinaxDc5l8kOT6nBw';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v11',
+        center: [106.69972796989238, -6.238601629433243],
+        zoom: 10
+    });
+
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false,
+        placeholder: 'Masukan kata kunci...',
+        zoom: 15
+    });
+
+    map.addControl(geocoder);
+
+    let marker = null
+
+    map.on('click', function(e) {
+        if(marker == null){
+            marker = new mapboxgl.Marker()
+                .setLngLat(e.lngLat)
+                .addTo(map);
+        } else {
+            marker.setLngLat(e.lngLat)
+        }
+
+        lk = e.lngLat
+        document.getElementById("coordinate").value = e.lngLat.lat+","+e.lngLat.lng;
+    });
+</script>
 @endsection
+{{-- @push('scripts')
+<script>
+    $(document).ready(function() {
+            $.ajax({
+                type: 'GET',
+                url: `{{ url('apotek') }}/${id}`,
+success: (res) => {
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hpbnlxMTEiLCJhIjoiY2ptY3d3OGxsMTA1dDNsbnl4OXJ1cHpkeCJ9.7fp_UEinaxDc5l8kOT6nBw';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/satellite-streets-v11',
+center: [res.data.longitude, res.data.latitude],
+zoom: 16
+});
+
+const marker1 = new mapboxgl.Marker()
+.setLngLat([res.data.longitude, res.data.latitude])
+.addTo(map);
+
+},
+error: function(data) {
+console.log(data);
+}
+});
+});
+</script>
+@endpush --}}
--- WebApp/resources/views/obat/index.blade.php
+++ WebApp/resources/views/obat/index.blade.php
@@ -60,7 +60,7 @@
                             <td style="width: 20%">{{ $obat->nama_obat }}</td>
                             <td style="width: 40%">{{ $obat->komposisi }}</td>
                             <td>
-                                <a href="obat/{{ $obat->id }}/detail" data-id="{{ $obat->id }}" class="detail btn btn-outline-primary">
+                                <a href="#" data-id="{{ $obat->id }}" class="detail btn btn-outline-primary">
                                     Detail
                                 </a>
                                 <a href="obat/{{ $obat->id }}/edit" class="btn btn-primary">
@@ -88,7 +88,52 @@
     });
 </script>
 @endsection
-
+<div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <h3 class="mt-4 mb-3 text-center" id="titleModal">Detail Museum</h3>
+            <div class="modal-body" style="padding-bottom: 5px">
+                <div class="form-group row mb-4">
+                    <div class="col-sm-10">
+                        <img id="image" class="mx-auto d-block" width="100" src="" alt="Photo Profile">
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label class="col-sm-2 col-form-label">Nama Obat</label>
+                    <div class="col-sm-10">
+                        <input id="nama" type="text" name="nama" class="form-control" disabled>
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label class="col-sm-2 col-form-label">Komposisi</label>
+                    <div class="col-sm-10">
+                        <input id="komposisi" type="text" name="komposisi" class="form-control" disabled>
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label class="col-sm-2 col-form-label">Aturan Pakai</label>
+                    <div class="col-sm-10">
+                        <input id="aturanPakai" type="text" name="aturanPakai" class="form-control" disabled>
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label class="col-sm-2 col-form-label">Khasiat</label>
+                    <div class="col-sm-10">
+                        <textarea id="khasiat" name="khasiat" class="form-control" style="height: 80px"
+                            disabled></textarea>
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label class="col-sm-2 col-form-label">Peringatan</label>
+                    <div class="col-sm-10">
+                        <textarea id="peringatan" name="peringatan" class="form-control" style="height: 80px"
+                            disabled></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="modal modal-danger fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="Delete"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -114,3 +159,34 @@
         </div>
     </div>
 </div>
+@push('scripts')
+<script>
+    $(document).ready(function() {
+            $(document).on('click', '.detail', function(event) {
+                event.preventDefault();
+                const id = $(this).data('id');
+                $.ajax({
+                    type: 'GET',
+                    url: `{{ url('obat') }}/${id}`,
+                    success: (res) => {
+                        $('#titleModal').html('Detail Obat')
+                        $('#nama').val(res.data.nama_obat);
+                        $('#peringatan').val(res.data.peringatan);
+                        $('#khasiat').val(res.data.khasiat);
+                        $('#aturanPakai').val(res.data.aturanPakai);
+                        $('#komposisi').val(res.data.komposisi);
+                        if(res.data.image){
+                            document.getElementById("image").src="storage/" + res.data.image;
+                        } else {
+                            document.getElementById("image").src="https://i.pravatar.cc/300?nocache='. microtime()";                         
+                        }
+                        $('#detailModal').modal('show');
+                    },
+                    error: function(data) {
+                        console.log(data);
+                    }
+                });
+            });
+        });
+</script>
+@endpush
