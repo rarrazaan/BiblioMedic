3c24543a1efe5f61eadf807dcdb2c4beae94e3c3
dapinda <arvindadsafira@gmail.com>
2022-06-16 22:03:21 +0700

    seeder obat apotek

--- WebApp/database/seeders/ObatApotekSeeder.php
+++ WebApp/database/seeders/ObatApotekSeeder.php
@@ -14,6 +14,436 @@ class ObatApotekSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 21000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 20000,
+                'qty' => 17 
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 22000,
+                'qty' => 10
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 21000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 21000,
+                'qty' => 5 
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 22000,
+                'qty' => 16
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'OBH Combi',
+                'harga' => 20000,
+                'qty' => 20  
+            ],
+
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 4500,
+                'qty' => 27  
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 5000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 4,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 6000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 5000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 6000,
+                'qty' => 14  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 5000,
+                'qty' => 16  
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'Antangin JRG',
+                'harga' => 7000,
+                'qty' => 16  
+            ],
+
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'Sanmol',
+                'harga' => 11000,
+                'qty' => 16  
+            ],
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'Sanmol',
+                'harga' => 12000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'Sanmol',
+                'harga' => 13000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'Sanmol',
+                'harga' => 11000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 4,
+                'nama_obat' => 'Sanmol',
+                'harga' => 13000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Sanmol',
+                'harga' => 11000,
+                'qty' => 10  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'Sanmol',
+                'harga' => 12000,
+                'qty' => 27  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Sanmol',
+                'harga' => 11500,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Sanmol',
+                'harga' => 13000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'Sanmol',
+                'harga' => 11000,
+                'qty' => 20  
+            ],
+
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 14  
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 16  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 10  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'Sanadryl Expectoran Sirup',
+                'harga' => 24000,
+                'qty' => 17  
+            ],
+
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 3000,
+                'qty' => 10  
+            ],
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 4000,
+                'qty' => 17  
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 3000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 3500,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 4,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 4000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 4500,
+                'qty' => 16  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 3000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 4000,
+                'qty' => 16  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 4000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'Sidomuncul Vitamin C 1000 Jeruk',
+                'harga' => 4000,
+                'qty' => 20  
+            ],
+            
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 16  
+            ],
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 10  
+            ],
+            [
+                'apotek_id' => 4,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 27  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 10  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Stimuno Syrup',
+                'harga' => 21000,
+                'qty' => 10  
+            ],
+            
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 16000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 17000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 17000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 16000,
+                'qty' => 14  
+            ],
+            [
+                'apotek_id' => 4,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 17000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 17500,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 16000,
+                'qty' => 5  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 17000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 16000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'Vicks Vaporub',
+                'harga' => 17000,
+                'qty' => 20  
+            ],
+            
+            
+            [
+                'apotek_id' => 0,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 26000,
+                'qty' => 14  
+            ],
+            [
+                'apotek_id' => 1,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 25000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 2,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 26000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 3,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 25000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 4,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 25000,
+                'qty' => 27  
+            ],
+            [
+                'apotek_id' => 5,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 26000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 6,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 25500,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 7,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 26000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 8,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 26000,
+                'qty' => 20  
+            ],
+            [
+                'apotek_id' => 9,
+                'nama_obat' => 'Komix Herbal',
+                'harga' => 25000,
+                'qty' => 20  
+            ],
+        ];
+        DB::table('apoteks')->insert($data);
     }
 }
